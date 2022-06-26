ed03e995b0d3dff9238cf42dc590bb74b23e1cb2
MJB <matt.berti13@gmail.com>
2022-06-26 14:00:20 -0500

    created thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,34 @@
 const { Schema, models, Types } = require('mongoose');
-const dateFormat = 
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAt => dateFormat(createdAt)
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
+
+// TODO: add virtuals to schema for reaction count
+
+const thought = model('Thought', ThoughtSchema);
+
+module.exports = thought;
