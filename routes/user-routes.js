c3eebb482f0342ab04555eae8ba67297979d44de
MJB <matt.berti13@gmail.com>
2022-06-26 12:34:42 -0500

    created user routes for JSON

---
+++ routes/user-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+
+const {
+    getAllUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+router
+.route('/')
+.get(getAllUser)
+.post(createUser)
+
+router
+.route('/:id')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser)
+
+router
+.route('/:userId/friends/:friendId')
+.put(addFriend)
+.delete(removeFriend)
+
+module.exports = router;
+
